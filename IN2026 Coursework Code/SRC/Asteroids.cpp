0d6211ca0f53e09b7f60d988fa68213098d2ca9d
Aaron-909 <Aaron.Bachu@city.ac.uk>
2024-04-23 03:52:23 +0100

    shield update
    managed to resize the shield image so it can be displayed. Still not fully functional yet.

--- IN2026 Coursework Code/SRC/Asteroids.cpp
+++ IN2026 Coursework Code/SRC/Asteroids.cpp
@@ -59,7 +59,7 @@ void Asteroids::Start()
 	Animation *asteroid1_anim = AnimationManager::GetInstance().CreateAnimationFromFile("asteroid1", 128, 8192, 128, 128, "asteroid1_fs.png");
 	Animation *spaceship_anim = AnimationManager::GetInstance().CreateAnimationFromFile("spaceship", 128, 128, 128, 128, "spaceship_fs.png");
 
-	Animation *shield_anim = AnimationManager::GetInstance().CreateAnimationFromFile("shield", 64, 64, 64, 64, "shield_fs.png");
+	Animation *shield_anim = AnimationManager::GetInstance().CreateAnimationFromFile("shield", 128, 128, 64, 64, "shield_fs.png");
 
 	// Create a spaceship and add it to the world
 	mGameWorld->AddObject(CreateSpaceship());
@@ -330,11 +330,11 @@ shared_ptr<GameObject> Asteroids::MakeShield(GLVector3f Position)
 	Animation *anim_ptr = AnimationManager::GetInstance().GetAnimationByName("shield");
 	shared_ptr<Sprite> shield_sprite =
 		make_shared<Sprite>(anim_ptr->GetWidth(), anim_ptr->GetHeight(), anim_ptr);
-	shield_sprite->SetLoopAnimation(true);
+	shield_sprite->SetLoopAnimation(false);
 	shared_ptr<GameObject> shield = make_shared<Shield>();
-	shield->SetBoundingShape(make_shared<BoundingSphere>(shield->GetThisPtr(), 10.0f));
+	shield->SetBoundingShape(make_shared<BoundingSphere>(shield->GetThisPtr(), 5.0f));
 	shield->SetSprite(shield_sprite);
-	shield->SetScale(0.02f);
+	shield->SetScale(0.25f);
 	shield->SetPosition(Position);
 	return shield;
 }
